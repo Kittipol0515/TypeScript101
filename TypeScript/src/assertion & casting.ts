cf81037b173c6e8efaee02984f6f1cea4a2f7312
Kittipol <kittipol.surapinchai@gmail.com>
2023-10-04 23:05:21 +0700

    update ts review

--- TypeScript/src/assertion & casting.ts
+++ TypeScript/src/assertion & casting.ts
@@ -10,3 +10,22 @@ let c = a as Three; // more specific type
 
 let d = <One>'world'
 let e = <string | number>'world'
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat') : number | string => {
+    if (c === 'add') return a + b;
+    return '' + a + b;
+}
+
+let myVal : string = addOrConcat(2,2,'concat') as string
+
+
+// Be careful TS no problem - but a string is returned
+let nextVal : number = addOrConcat(2,2,'concat') as number
+
+// 10 as string
+(10 as unknown) as string
+
+// The DOM
+
+const img = document.querySelector('img')
+const myImg = document.getElementById('#img')
--- TypeScript/src/destructuring.ts
+++ TypeScript/src/destructuring.ts
@@ -1,6 +1,6 @@
 //array
 
 const colors1:string[] = ["red", "green", "yellow"]
-const [a,b,c] = colors1
+// const [a,b,c] = colors1
 
 console.log(a,b,c)
